d63b5eea9175e3dfe2e55818749b2f564e5ec98e
Valente Arroyo <VXA2502@yum.com>
2021-08-09 16:20:18 -0500

    Updated and made sure UI works correctly with new recyclerView, updating strings to keep up with information and bullet points

--- app/src/main/java/com/example/finalpresentation/DetailsProvider.kt
+++ app/src/main/java/com/example/finalpresentation/DetailsProvider.kt
@@ -24,34 +24,34 @@ class DetailsProvider {
 
     fun loadKotlinQuestions(): List<LessonDetailQuestion> {
         return listOf(
-            LessonDetailQuestion(R.string.intro_title, R.string.intro_answer),
-            LessonDetailQuestion(R.string.self_intro_title, R.string.self_intro_answer),
-            LessonDetailQuestion(R.string.android_q_one, R.string.android_a_one)
+            LessonDetailQuestion(R.string.kotlin_q_one, R.string.kotlin_a_one),
+            LessonDetailQuestion(R.string.kotlin_q_two, R.string.kotlin_a_two),
+            LessonDetailQuestion(R.string.kotlin_q_three, R.string.kotlin_a_three)
         )
     }
 
     fun loadXMLQuestions(): List<LessonDetailQuestion> {
         return listOf(
-            LessonDetailQuestion(R.string.intro_title, R.string.intro_answer),
-            LessonDetailQuestion(R.string.self_intro_title, R.string.self_intro_answer),
-            LessonDetailQuestion(R.string.android_q_one, R.string.android_a_one)
+            LessonDetailQuestion(R.string.xml_q_one, R.string.xml_a_one),
+            LessonDetailQuestion(R.string.xml_q_two, R.string.xml_a_two),
+            LessonDetailQuestion(R.string.xml_q_three, R.string.xml_a_three)
         )
     }
 
     fun loadBindingQuestions(): List<LessonDetailQuestion> {
         return listOf(
-            LessonDetailQuestion(R.string.intro_title, R.string.intro_answer),
-            LessonDetailQuestion(R.string.self_intro_title, R.string.self_intro_answer),
-            LessonDetailQuestion(R.string.android_q_one, R.string.android_a_one)
+            LessonDetailQuestion(R.string.binding_q_one, R.string.binding_a_one),
+            LessonDetailQuestion(R.string.binding_q_two, R.string.binding_a_two),
+            LessonDetailQuestion(R.string.binding_q_three, R.string.binding_a_three)
         )
     }
 
     fun loadGitQuestions(): List<LessonDetailQuestion> {
         return listOf(
-            LessonDetailQuestion(R.string.intro_title, R.string.intro_answer),
-            LessonDetailQuestion(R.string.self_intro_title, R.string.self_intro_answer),
-            LessonDetailQuestion(R.string.android_q_one, R.string.android_a_one)
-
+            LessonDetailQuestion(R.string.git_q_one, R.string.git_a_one),
+            LessonDetailQuestion(R.string.git_q_two, R.string.git_a_two),
+            LessonDetailQuestion(R.string.git_q_three, R.string.git_a_three),
+            LessonDetailQuestion(R.string.git_q_four, R.string.git_a_four)
         )
     }
 
--- app/src/main/java/com/example/finalpresentation/LessonsProvider.kt
+++ app/src/main/java/com/example/finalpresentation/LessonsProvider.kt
@@ -5,13 +5,13 @@ class LessonsProvider {
         return listOf(
             Lesson(R.string.presentation_intro, R.drawable.android_half_trident, R.drawable.poseidon_text, DetailsProvider().loadIntroQuestions()),
             Lesson(R.string.presentation_self_intro, R.drawable.silhouette,R.drawable.varr, DetailsProvider().loadSelfIntroQuestions()),
+            Lesson(R.string.presentation_git, R.drawable.git, R.drawable.git_folder, DetailsProvider().loadGitQuestions()),
+            Lesson(R.string.presentation_jira_agile, R.drawable.jira,R.drawable.jira_tickets, DetailsProvider().loadJiraQuestions()),
             Lesson(R.string.presentation_android, R.drawable.android_present,R.drawable.android_taco, DetailsProvider().loadAndroidQuestions()),
             Lesson(R.string.presentation_kotlin, R.drawable.kotlin,R.drawable.android_kotlin, DetailsProvider().loadKotlinQuestions()),
             Lesson(R.string.presentation_xml, R.drawable.xml_presentation,R.drawable.xml_android, DetailsProvider().loadXMLQuestions()),
-            Lesson(R.string.presentation_binding, R.drawable.view_binding,R.drawable.view_binding_details, DetailsProvider().loadBindingQuestions()),
-            Lesson(R.string.presentation_git, R.drawable.git, R.drawable.git_folder, DetailsProvider().loadGitQuestions()),
-            Lesson(R.string.presentation_jira_agile, R.drawable.jira,R.drawable.jira_tickets, DetailsProvider().loadJiraQuestions()),
             Lesson(R.string.presentation_architecture, R.drawable.mvmpattern,R.drawable.user_mvvm, DetailsProvider().loadMVVMQuestions()),
+            Lesson(R.string.presentation_binding, R.drawable.view_binding,R.drawable.view_binding_details, DetailsProvider().loadBindingQuestions()),
             Lesson(R.string.presentation_pos, R.drawable.yum_dev_white, R.drawable.yum_dev, DetailsProvider().loadPOSQuestions())
         )
     }
